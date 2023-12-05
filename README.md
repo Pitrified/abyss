162fb402afa022c6360e237b29e4a305920299e1
Pitrified <nobilipietro@gmail.com>
2023-12-05 20:28:02 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Stare into the Abyss
+
+1. Compute the position of the viewer
+1. Compute the position of the screen 
+1. Render the scene the viewer sees on the screen
---
+++ src/abyss/utils/data.py
@@ -0,0 +1,41 @@
+"""Utility functions dealing with IO."""
+
+from pathlib import Path
+from typing import Literal
+
+from loguru import logger as lg
+
+
+def get_resource(
+    which_res: Literal[
+        "pose_landmarker.task",
+        "root_fol",
+        "sample_fol",
+        "3D_model_fol",
+    ]
+) -> Path:
+    """Get the path of the requested resource."""
+    # resources
+    if which_res == "pose_landmarker.task":
+        mp_model_fol = Path.home() / ".mediapipe" / "models"
+        pose_landmark_model_path = mp_model_fol / "pose_landmarker.task"
+        return pose_landmark_model_path
+
+    # folders that are not in the package
+    if which_res == "3D_model_fol":
+        return Path.home() / "data" / "3d_models"
+
+    # folders that are in the package
+    if which_res == "root_fol":
+        return Path(__file__).absolute().parents[3]
+    elif which_res == "sample_fol":
+        return get_resource("root_fol") / "data" / "sample"
+
+
+def check_create_fol(
+    fol: Path,
+) -> None:
+    """Check if folder exists, if not create it."""
+    if not fol.exists():
+        lg.debug(f"Creating folder {fol}")
+        fol.mkdir(parents=True)
